5a43134e921ee81b46208f47192d79a0be24a119
MEng <lymeng.pen@suntel.io>
2025-06-09 11:33:00 +0700

    Update

--- controllers/questions/listQuestion.js
+++ controllers/questions/listQuestion.js
@@ -3,7 +3,15 @@ const db = require("../../models")
 const listQuestion = async (req, res, next)=>{
     const resp =  await db.Question.findAll();
 
+    
+    sleep(8000);
+
     return res.json(resp)
+}
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 
 
